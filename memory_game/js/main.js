946b1c87fa15937f308dcaeacf30bfbc1f3270e0
jooewoo <jooewoo@gmail.com>
2018-10-18 21:01:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,18 @@
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again!");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again!");
+	}
 };
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId){
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	console.log("User flipped " + cardsInPlay[cardId]);
+};
+flipCard(0);
+flipCard(2);
+
 
